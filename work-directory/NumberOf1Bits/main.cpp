a7b60ae0d8b89666ad43745244d5e3abb4170a24
Eugene Mavrin <e.mavrin@skoltech.ru>
2021-03-05 18:55:50 +0300

    LC-191

---
+++ work-directory/NumberOf1Bits/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        // Runtime: 0 ms
+        // Memory Usage: 5.9 MB
+        int res = 0;
+        while (n > 0) {
+            res += n % 2;
+            n /= 2;
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    Solution s;
+    cout << s.hammingWeight(15) << endl;
+    return 0;
+}
