c35053bfb4a4e0f8fd9607d8316344e0ea0caed3
Eugene Mavrin <e.mavrin@skoltech.ru>
2021-03-07 00:02:12 +0300

    LC-820

--- 820. Short Encoding of Words/main.cpp
+++ 820. Short Encoding of Words/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class Solution {
 public:
-    int minimumLengthEncoding(vector<string>& words) {
+    int minimumLengthEncoding_(vector<string>& words) {
         // Runtime: 1164 ms,      out of range
         // Memory Usage: 17.7 MB, 68%
         unordered_set<string> set;
@@ -34,12 +34,31 @@ public:
         }
         return result;
     }
+
+    int minimumLengthEncoding(vector<string>& words) {
+        // Runtime: 40 ms,        90%
+        // Memory Usage: 17.4 MB, 74%
+        unordered_set<string> set(words.begin(), words.end());
+
+        for (const auto &s : set) {
+            int len = s.length();
+            for (int i = 1; i < len; i++) {
+                set.erase(s.substr(len - i, i));
+            }
+        }
+
+        int result = 0;
+        for (const auto& s : set) {
+            result+=s.length() + 1;
+        }
+        return result;
+    }
 };
 
 int main()
 {
     Solution s;
-    vector<string> words( {"me", "time"} );
+    vector<string> words( {"time", "me", "bell"} );
     cout << s.minimumLengthEncoding(words) << endl;
     return 0;
 }
