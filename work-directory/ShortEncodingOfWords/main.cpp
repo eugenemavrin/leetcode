1a7d42013104c06549729116ef1a085922c1208b
Eugene Mavrin <e.mavrin@skoltech.ru>
2021-03-06 22:33:56 +0300

    LC-820

---
+++ work-directory/ShortEncodingOfWords/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumLengthEncoding(vector<string>& words) {
+        // Runtime: 1164 ms,      out of range
+        // Memory Usage: 17.7 MB, 68%
+        unordered_set<string> set;
+        for (auto w : words) {
+            w.append("#");
+            bool isContains = false;
+            for (const auto& s : set) {
+                if (s.find(w) != std::string::npos) {
+                    isContains = true;
+                    break;
+                }
+                if (w.find(s) != std::string::npos) {
+                    set.erase(s);
+                    break;
+                }
+            }
+            if (!isContains) {
+                set.insert(w);
+            }
+        }
+
+        int result = 0;
+        for (const auto& s : set) {
+            result+=s.length();
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<string> words( {"me", "time"} );
+    cout << s.minimumLengthEncoding(words) << endl;
+    return 0;
+}
