55d6d2684356af0b3893484d6be1f6adf5a6d2db
Eugene Mavrin <e.mavrin@skoltech.ru>
2021-03-09 00:49:53 +0300

    LC-706

---
+++ work-directory/DesignHashMap/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class MyHashMap {
+    // Runtime: 108 ms,        77%
+    // Memory Usage: 188.6 MB, 23%
+public:
+    /** Initialize your data structure here. */
+    MyHashMap() {
+        mValues = vector<int>(1000000, -1);
+    }
+
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        mValues.at(key) = value;
+    }
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        return mValues.at(key);
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        mValues.at(key) = -1;
+    }
+private:
+    vector<int> mValues;
+};
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
