6b250ca406eb5b0ef0b8229d44e3e50e582c616f
Eugene Mavrin <e.mavrin@skoltech.ru>
2021-03-09 10:53:09 +0300

    LC-1

---
+++ work-directory/TwoSum/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // Runtime: 4 ms,        97%
+        // Memory Usage: 8.8 MB, 96%
+        int len = nums.size();
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (nums[i] + nums[j] == target)
+                    return {i, j};
+            }
+        }
+        return {};
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> nums = { -3, 4, 3, 90 };
+    const auto res = s.twoSum(nums, 0);
+    cout << "Hello World!" << endl;
+    return 0;
+}
